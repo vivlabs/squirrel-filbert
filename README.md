2c57cda83208518869714accd752b4efb33ca926
Michael Blakeley <mblakele@viv.ai>
2016-09-14 13:45:04 -0700

    working prototype

---
+++ README.md
@@ -0,0 +1,64 @@
+# Simple S3 Proxy for Squirrel Updates
+
+This nodejs package implements a
+[squirrel update service]((https://github.com/Squirrel/Squirrel.Mac#update-requests)
+using Amazon S3 as a backend. Your builds and build metadata live in
+S3.  As update requests arrive, the server checks a well-known S3 bucket and key
+for a `latest.json` file, and uses this data to decide whether to send 204
+if the client is already up to date, or JSON based on `latest.json` if
+the client should update.
+
+## Installation
+
+Clone this repo or download a release, set environment variables
+according to "Configuration" below, then `npm start`.
+
+To make the server useful, you'll need to upload a `latest.json` to
+your channel bucket as specified by `FILBERT_CHANNEL_BUCKET` and
+`FILBERT_CHANNEL_BUCKET`. Here's a sample:
+
+```json
+{
+  "build": "fubar.dmg",
+  "name": "Fubar Pro",
+  "notes": "Warning: processed in a facility that also processes nuts.",
+  "pub_date": "2016-09-14T12:34:56-00:00",
+  "version": "0.1.2"
+}
+```
+
+Filbert uses [semver](http://semver.org/) to compare the client's
+version string with the server's version string.
+
+Sample requests and responses:
+
+* `curl -i 'http://localhost:3000/status/test/0.0.1'`: 200, because
+  0.0.1 has an update available.
+* `curl -i 'http://localhost:3000/status/test/0.1.2'`: 204, no update
+  available.
+* `curl -i 'http://localhost:3000/status/test/0'`: 400, because `0` is
+  not a semver.
+
+When an update is available, the response includes a url built from
+the `FILBERT_BUILD_BUCKET`, `FILBERT_BUILD_PREFIX`, and the `build`
+from that channel's `latest.json`.
+
+## Configuration
+
+Filbert uses environment variables for its configuration.
+
+* `FILBERT_PORT`: The listener port, default 3000.
+* `FILBERT_BUILD_BUCKET`: The S3 bucket where builds live.
+* `FILBERT_BUILD_PREFIX`: The prefix under `FILBERT_BUILD_BUCKET`
+  where builds live. This can be empty. If non-empty, end it with `/`.
+* `FILBERT_CHANNEL_BUCKET`: The S3 bucket where channel information
+  lives.
+* `FILBERT_CHANNEL_PREFIX`: The prefix under `FILBERT_CHANNEL_BUCKET`
+  where each channel's `latest.json` lives. This can be empty. If
+  non-empty, end it with `/`.
+
+For AWS credentials configure a `$HOME/.aws/credentials`, AWS
+environment credentials, or IAM profile with GetObject access to the
+appropriate S3 content.
+
+# The End
---
+++ index.js
@@ -0,0 +1,150 @@
+// index.js
+
+const AWS = require('aws-sdk')
+const bodyParser = require('body-parser')
+const express = require('express')
+const http = require('http')
+const semver = require('semver')
+
+const app = express()
+app.use(bodyParser.json({limit: '1kb'}))
+
+const envDefaults = {
+  FILBERT_PORT: 3000,
+  FILBERT_SCHEME: 'http'
+}
+
+const envMerged = Object.assign({}, envDefaults, process.env)
+
+const {
+  FILBERT_BUILD_BUCKET,
+  FILBERT_BUILD_PREFIX,
+  FILBERT_CHANNEL_BUCKET,
+  FILBERT_CHANNEL_PREFIX,
+  FILBERT_PORT,
+  FILBERT_SCHEME
+} = envMerged
+
+if (!FILBERT_BUILD_BUCKET) {
+  console.error('Must set FILBERT_BUILD_BUCKET')
+  process.exit(-1)
+}
+
+if (FILBERT_BUILD_PREFIX && !FILBERT_BUILD_PREFIX.endsWith('/')) {
+  console.error('If set, FILBERT_BUILD_PREFIX must end with "/"')
+  process.exit(-1)
+}
+
+if (FILBERT_BUILD_PREFIX && FILBERT_BUILD_PREFIX.startsWith('/')) {
+  console.error('If set, FILBERT_BUILD_PREFIX must not start with "/"')
+  process.exit(-1)
+}
+
+if (!FILBERT_CHANNEL_BUCKET) {
+  console.error('Must set FILBERT_CHANNEL_BUCKET')
+  process.exit(-1)
+}
+
+if (FILBERT_CHANNEL_PREFIX && !FILBERT_CHANNEL_PREFIX.endsWith('/')) {
+  console.error('If set, FILBERT_CHANNEL_PREFIX must end with "/"')
+  process.exit(-1)
+}
+
+if (FILBERT_CHANNEL_PREFIX && FILBERT_CHANNEL_PREFIX.startsWith('/')) {
+  console.error('If set, FILBERT_CHANNEL_PREFIX must not start with "/"')
+  process.exit(-1)
+}
+
+if (FILBERT_SCHEME !== 'http' && FILBERT_SCHEME !== 'https') {
+  console.error('Bad value for FILBERT_SCHEME', FILBERT_SCHEME)
+  process.exit(-1)
+}
+
+app.use('/build/:buildId', function (req, res, next) {
+  const { buildId } = req.params
+
+  // Expect .aws/credentials, environment credentials, or IAM profile.
+  const s3 = new AWS.S3()
+  const bucket = FILBERT_BUILD_BUCKET
+  const key = FILBERT_BUILD_PREFIX + buildId
+
+  s3.getObject(
+    {
+      Bucket: bucket,
+      Key: key
+    }
+  ).on('httpHeaders', function (statusCode, headers) {
+    if (statusCode !== 200) {
+      console.error(bucket, key, statusCode)
+    }
+
+    res.status(statusCode)
+    Object.keys(headers).forEach((name) => {
+      if (name === 'Body' || name === 'Metadata') { return }
+      res.append(name, headers[name])
+    })
+    this.response.httpResponse.createUnbufferedStream().pipe(res)
+  }).send()
+})
+
+app.use('/status/:channel/:version', function (req, res, next) {
+  const { channel, version } = req.params
+  if (!semver.valid(version)) {
+    console.error('bad version', version, 'on channel', channel)
+    return res.status(400).send('invalid version')
+  }
+
+  // Expect .aws/credentials, environment credentials, or IAM profile.
+  const s3 = new AWS.S3()
+  const bucket = FILBERT_CHANNEL_BUCKET
+  const key = FILBERT_CHANNEL_PREFIX + channel + '/latest.json'
+
+  s3.getObject(
+    {
+      Bucket: bucket,
+      Key: key
+    },
+    function (err, data) {
+      if (err) {
+        console.error(channel, version, bucket, key, err, err.stack)
+        if (err.statusCode) {
+          return res.status(err.statusCode).send(err.message)
+        }
+        return res.status(500).send(err)
+      }
+
+      const latest = JSON.parse(data.Body.toString())
+      if (!latest || !latest.version) {
+        console.error('channel', channel, 'has bad data', data)
+        return res.status(500)
+          .send('bad data in channel ' + channel + ': ' + data)
+      }
+      if (!semver.valid(latest.version)) {
+        console.error('channel', channel, 'has bad version data', version)
+        return res.status(500)
+          .send('bad version data in channel ' + channel + ': ' + version)
+      }
+
+      if (semver.gte(version, latest.version)) {
+        // Already up to date.
+        return res.sendStatus(204)
+      }
+
+      const { build, name, notes, pub_date } = latest
+      if (!build) {
+        console.error('channel', channel, 'has bad data', latest)
+        return res.status(500)
+          .send('bad data in channel ' + channel + ': ' + data)
+      }
+      const url = FILBERT_SCHEME + '://' +
+            (req.get('host') || ('localhost' + FILBERT_PORT)) +
+            '/build/' + build
+      return res.send({name, notes, pub_date, url})
+    }
+  )
+})
+
+http.createServer(app).listen(FILBERT_PORT)
+console.log('filbert is listening on port', FILBERT_PORT)
+
+// end
